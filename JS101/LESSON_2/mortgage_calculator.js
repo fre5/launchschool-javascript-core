8aa49c4cd85ae6c06e6ed6624ce7abac680f27d9
fre5 <enjsof@gmail.com>
2021-03-04 08:36:50 -0800

    used coercion to read string on input variable and added repeat calculation loop

--- JS101/LESSON_2/mortgage_calculator.js
+++ JS101/LESSON_2/mortgage_calculator.js
@@ -2,28 +2,37 @@ const rlSync = require('readline-sync');
 
 console.log("Welcome to Mortgage Calculator!");
 
-let askValidate = (message) => {
-  let input;
-  do  {
-    input = parseFloat(rlSync.question(`Please enter the ${message}`));
-  } while (isNaN(input));
-  return input;
-};
+let repeat; //variable used to determine state of repeating the calculation
 
-let loanAmount = askValidate('loan amount: ');
-let annualRate = askValidate('Annual Percentage Rate (in percentage): ') / 100;
-let loanDuration = askValidate('loan duration (in year(s)): ') * 12;
-let monthlyRate = annualRate / 12;
+do {
+  let askValidate = (message) => {
+    let input;
+    do  {
+      input = rlSync.question(`Please enter the ${message}`);
+    } while (isNaN(input));
+    return parseFloat(input);
+  };
 
-let monthlyPayment;
+  let loanAmount = askValidate('loan amount: ');
+  let annualRate = askValidate('Annual Percentage Rate (in percentage): ') / 100;
+  let loanDuration = askValidate('loan duration (in year(s)): ') * 12;
+  let monthlyRate = annualRate / 12;
 
-if (monthlyRate !== 0) {
-  let denominator = 1 - Math.pow((1 + monthlyRate), (-loanDuration));
-  monthlyPayment = loanAmount * (monthlyRate / denominator);
-} else {
-  monthlyPayment = loanAmount / loanDuration;
-}
+  let monthlyPayment;
 
-console.log(`Your monthly payment is ${monthlyPayment.toFixed(2)}`);
+  if (monthlyRate !== 0) {
+    let denominator = 1 - Math.pow((1 + monthlyRate), (-loanDuration));
+    monthlyPayment = loanAmount * (monthlyRate / denominator);
+  } else {
+    monthlyPayment = loanAmount / loanDuration;
+  }
+
+  console.log(`Your monthly payment is ${monthlyPayment.toFixed(2)}`);
+  do {
+    repeat = rlSync.question("Would you like to make another calculation? (y/n) ").toLowerCase();
+  } while (repeat !== 'y' && repeat !== 'n');
+} while (repeat === 'y');
+
+console.log("Thank you and goodbye!");
 
 
