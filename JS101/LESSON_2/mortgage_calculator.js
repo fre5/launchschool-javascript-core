67c625c7be4b865eb532072767828143abf5359a
fre5 <enjsof@gmail.com>
2021-03-04 10:18:08 -0800

    refactored and moved loanDuration conditional right after being  declared

--- JS101/LESSON_2/mortgage_calculator.js
+++ JS101/LESSON_2/mortgage_calculator.js
@@ -2,9 +2,6 @@ const rlSync = require('readline-sync');
 
 console.log("Welcome to Mortgage Calculator!");
 
-let repeat; //variable used to determine state of repeating the calculation
-let monthlyPayment;
-
 let askValidate = (message) => {
   let input;
   do  {
@@ -13,24 +10,31 @@ let askValidate = (message) => {
   return parseFloat(input);
 };
 
-do {
+while (true) {
   let loanAmount = askValidate('loan amount: ');
   let annualRate = askValidate('Annual Percentage Rate (in percentage): ') / 100;
   let loanDuration = askValidate('loan duration (in year(s)): ') * 12;
+  loanDuration = loanDuration === 0 ? 1 : '';
   let monthlyRate = annualRate / 12;
+  let monthlyPayment;
+  let repeat;
 
   if (monthlyRate !== 0) {
     let denominator = 1 - Math.pow((1 + monthlyRate), (-loanDuration));
     monthlyPayment = loanAmount * (monthlyRate / denominator);
   } else {
-    monthlyPayment = loanAmount / (loanDuration === 0 ? 1 : '');
+    monthlyPayment = loanAmount / loanDuration;
   }
 
   console.log(`Your monthly payment is ${monthlyPayment.toFixed(2)}`);
-  do {
+
+  while (true) {
     repeat = rlSync.question("Would you like to make another calculation? (y/n) ").toLowerCase();
-  } while (repeat !== 'y' && repeat !== 'n');
-} while (repeat === 'y');
+    if (repeat === 'y' || repeat === 'n') break;
+  }
+
+  if (repeat === 'n') break;
+}
 
 console.log("Thank you and goodbye!");
 
