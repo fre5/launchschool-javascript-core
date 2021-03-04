7f9f5852af7ee73d744a6410d72a337c54b51e60
fre5 <enjsof@gmail.com>
2021-03-04 08:08:57 -0800

    linted

--- JS101/LESSON_2/mortgage_calculator.js
+++ JS101/LESSON_2/mortgage_calculator.js
@@ -6,19 +6,20 @@ let askValidate = (message) => {
   let input;
   do  {
     input = parseFloat(rlSync.question(`Please enter the ${message}`));
-  } while(isNaN(input))
+  } while (isNaN(input));
   return input;
-}
+};
 
 let loanAmount = askValidate('loan amount: ');
-let annualRate = askValidate('Annual Percentage Rate (in percentage): ') /100;
+let annualRate = askValidate('Annual Percentage Rate (in percentage): ') / 100;
 let loanDuration = askValidate('loan duration (in year(s)): ') * 12;
 let monthlyRate = annualRate / 12;
 
 let monthlyPayment;
 
-if(monthlyRate !== 0) {
-  monthlyPayment = loanAmount * (monthlyRate / (1 - Math.pow((1 + monthlyRate), (-loanDuration))));
+if (monthlyRate !== 0) {
+  let denominator = 1 - Math.pow((1 + monthlyRate), (-loanDuration));
+  monthlyPayment = loanAmount * (monthlyRate / denominator);
 } else {
   monthlyPayment = loanAmount / loanDuration;
 }
