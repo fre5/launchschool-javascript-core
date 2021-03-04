c8bede8594795022987308f1b5d03c0c11559ba0
fre5 <enjsof@gmail.com>
2021-03-04 08:58:47 -0800

    added edge case input is an empty string and took out askValidate function out of the do while loop

--- JS101/LESSON_2/mortgage_calculator.js
+++ JS101/LESSON_2/mortgage_calculator.js
@@ -3,22 +3,21 @@ const rlSync = require('readline-sync');
 console.log("Welcome to Mortgage Calculator!");
 
 let repeat; //variable used to determine state of repeating the calculation
+let monthlyPayment;
+
+let askValidate = (message) => {
+  let input;
+  do  {
+    input = rlSync.question(`Please enter the ${message}`);
+  } while (isNaN(input) || input === '');
+  return parseFloat(input);
+};
 
 do {
-  let askValidate = (message) => {
-    let input;
-    do  {
-      input = rlSync.question(`Please enter the ${message}`);
-    } while (isNaN(input));
-    return parseFloat(input);
-  };
-
   let loanAmount = askValidate('loan amount: ');
   let annualRate = askValidate('Annual Percentage Rate (in percentage): ') / 100;
   let loanDuration = askValidate('loan duration (in year(s)): ') * 12;
   let monthlyRate = annualRate / 12;
-
-  let monthlyPayment;
 
   if (monthlyRate !== 0) {
     let denominator = 1 - Math.pow((1 + monthlyRate), (-loanDuration));
